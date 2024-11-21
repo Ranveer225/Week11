03ca1b186dbdb8273cab4a588ada175b8bf7ad4a
Ranveer225 <singr12@farmingdale.edu>
2024-11-20 19:35:38 -0500

    ToDo 3: Start application using CountDownActivity

--- app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
@@ -36,7 +36,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 
-
 // ToDo 1: Call `FibonacciDemoNoBgThrd` that calculates the Fibonacci number of a given number.
 // ToDo 2: Create a composable function called `FibonacciDemoWithCoroutine` that calculates the
 //  Fibonacci number of a given number using a coroutine.
